c96dff18c4900c9bf6e09404b24049dd2900d568
Hugo Ratte <hugo.ratte@epita.fr>
2023-03-30 17:13:41 +0200

    feat(App) : possibility to have details of game && the operations will not change if the input is not an integer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,6 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { red, green } from 'ansi-colors';
 
-const Counter = (good) => {
-  return (
-    good + 1
-  );
-}
-
 const resultOperation = (operations) => {
   const [num1, operator, num2] = operations.split(' ');
 
@@ -37,39 +31,77 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [game, setGame] = useState(0);
   const [selectedMode, setSelectedMode] = useState(null);
-  const [operations, setOperations] = useState("5 * 5 = ?");
+  const [operations, setOperations] = useState("");
   const [isGameFinished, setIsGameFinished] = useState(false);
-
-
-  const Response = async (result) => {
-    const response = await fetch('https://api.github.com/users');
-    const data = await response.json();
-    console.log(data);
-  }
-
+  const [listQuestions, setListQuestions] = useState([]);
+  const [listResponses, setListResponses] = useState([]);
+  const [Details, setDetails] = useState(false);
 
   const handleSubmit = () => {
-    setGame(game + 1);
     const result = resultOperation(operations);
-    if (parseInt(inputValue) === result) {
+    if (inputValue === '') {
+      console.log("Please enter a number");
+      setCounter(2);
+    }
+    else if (parseInt(inputValue) === result) {
+      setGame(game + 1);
+      listResponses.push("Well done! Your answer " + inputValue + " is correct for " + operations);
       setGood(good + 1);
       setCounter(1);
-    } else {
+    }
+    else {
+      setGame(game + 1);
+      listResponses.push("Your answer " + inputValue + " is wrong for " + operations);
       setCounter(-1);
     }
-    if (game === 9)
-    {
+    if (game === 9) {
       setIsGameFinished(true);
+      setListQuestions([]);
     }
 
   }
 
-  const handleNewOperation = () => {
+  const handleNewOperationEasy = () => {
     const num1 = Math.floor(Math.random() * (10 - 0 + 1)) + 0
     const num2 = Math.floor(Math.random() * (10 - 0 + 1)) + 0
     const op = num1 + " + " + num2 + " = ?";
+    if (listQuestions.includes(op)) {
+      console.log("doublon");
+      handleNewOperationEasy();
+    }
+    listQuestions.push(op);
     setOperations(op);
     setInputValue('');
+  }
+
+  const handleNewOperationHard = () => {
+    let num1 = Math.floor(Math.random() * (10 - 0 + 1)) + 0;
+    let num2 = Math.floor(Math.random() * (20 - 0 + 1)) + 0;
+    const random = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
+    let op = '';
+    if (random === 0) {
+      op = num1 + " * " + num2 + " = ?";
+    }
+    else {
+      num1 = Math.floor(Math.random() * (30 - 20 + 1)) + 20;
+      num2 = Math.floor(Math.random() * (20 - 10 + 1)) + 10;
+      op = num1 + " - " + num2 + " = ?";
+    }
+
+    if (listQuestions.includes(op)) {
+      console.log("doublon");
+      handleNewOperationHard();
+    }
+    listQuestions.push(op);
+    setOperations(op);
+    setInputValue('');
+  }
+
+  const handleNewOperation = () => {
+    if (selectedMode === 'easy')
+      handleNewOperationEasy();
+    else if (selectedMode === 'hard')
+      handleNewOperationHard();
   }
 
   useEffect(() => {
@@ -79,14 +111,21 @@ const App = () => {
         setCounter(0);
       }, 600);
     }
+    if (counter === 2) {
+      setTimeout(() => {
+        setCounter(0);
+      }, 600);
+    }
   }, [counter]);
 
   const handleEasyMode = () => {
     setSelectedMode('easy');
+    handleNewOperationEasy();
   };
 
   const handleHardMode = () => {
     setSelectedMode('hard');
+    handleNewOperationHard();
   };
 
   const result = resultOperation(operations);
@@ -95,7 +134,7 @@ const App = () => {
     <div className="App">
       <h1>Calculus</h1>
       {!selectedMode && (
-        <div>
+        <div className='Mode'>
           <h2>Choose a mode:</h2>
           <button onClick={handleEasyMode}>Easy</button>
           <button onClick={handleHardMode}>Hard</button>
@@ -103,28 +142,56 @@ const App = () => {
       )}
       {selectedMode && game < 10 && (
         <div className="Operation">
-          <h1>{operations}</h1>
-          <input type="number" value={inputValue} onChange={(e) => setInputValue(e.target.value)} 
-          onKeyDown={(e) => e.key === 'Enter' && handleSubmit()} 
+          <h2>{operations}</h2>
+          <input type="number" value={inputValue} onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={(e) => e.key === 'Enter' && handleSubmit()}
           />
           <button onClick={handleSubmit}>Valid</button>
-          <h4 style={{ color: counter === 1 ? 'green' : 'red' }}>
-            {counter === 1 ? 'TRUE' : counter === -1 ? result : ""}
-          </h4>
+          <h5 style={{ color: counter === 1 ? 'green' : 'red' }}>
+            {
+              counter === 1 ?
+                'TRUE' : counter === -1 ?
+                  result : counter === 2 ? "Please enter a number" : ""}
+          </h5>
         </div>
       )}
-      {isGameFinished && (
+      {!Details && isGameFinished && (
         <div className='Result'>
-          <h3>Good answers: {good}</h3>
-          <h3>Bad answers: {game - good}</h3>
+          <h2>Good answers: {good}</h2>
+          <h2>Bad answers: {game - good}</h2>
           <button onClick={() => {
             setIsGameFinished(false);
             setGame(0);
             setGood(0);
+            setSelectedMode('');
             handleNewOperation();
           }}>Rejouer</button>
+
+          <button onClick={() => {
+            setDetails(true);
+          }}>Details</button>
         </div>
       )}
+      {Details && (
+        <div className='Details'>
+          <h6>
+            {listResponses.map((item, index) => (
+              <p key={index + "baba"}>{item}</p>
+            ))}
+          </h6>
+          <button onClick={() => {
+            setIsGameFinished(false);
+            setGame(0);
+            setGood(0);
+            setSelectedMode('');
+            handleNewOperation();
+            setDetails(false);
+            setListResponses([]);
+          }}>Rejouer</button>
+
+        </div>
+      )}
+
     </div>
   );
 }
